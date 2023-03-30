f825abdfbdf1dddeabfa2fbf3e17aa73662683a3
Deepak Ghimire <deepak.ghimire@sentera.com>
2023-03-30 12:57:33 -0500

    interp model script

---
+++ scripts/interpolation_methods.py
@@ -0,0 +1,63 @@
+# %% Import libraries and data
+import numpy as np
+import pandas as pd
+from pandas import read_csv
+from scipy.interpolate import Akima1DInterpolator, CubicSpline, PchipInterpolator
+
+# %% Load data
+# TODO: Put data on cloud drive
+
+df_allyears_in = read_csv("/root/git/demeter-utils/ndvi_gimms_allyears.csv")
+df_allyears_in["year"] = pd.DatetimeIndex(df_allyears_in["START DATE"]).year
+
+# %% Clean data
+# create a new column for year and other column for MMDD
+df_allyears = df_allyears_in[df_allyears_in["SAMPLE_VALUE"].notna()]
+# df_2000_NAdropped = df_allyears_NAdropped[df_allyears_NAdropped['year']==2000]
+
+# %% create a list of years
+# years = np.unique(df_allyears_NAdropped['year'].values)
+
+x_new = np.linspace(1, 365, num=365, dtype=int)
+
+# import other interpolation methods from scipy.interpolate
+# create x-axis values for interpolation
+x_new = np.linspace(1, 365, num=365, dtype=int)
+# Plot the new interpolated values obtained using different methods
+
+# %% Loop through each year and set two new columns: interp_type and interp_val
+
+models_by_year = {}
+
+for year in df_allyears["year"].unique():
+    # filter df_allyears by year
+    df = df_allyears[df_allyears["year"] == year]
+    for Model, name in [
+        (CubicSpline, "cubic_spline"),
+        (Akima1DInterpolator, "akima_1d_interpolator"),
+        (PchipInterpolator, "pchip_interpolator"),
+    ]:
+        model = Model(x=df["DOY"], y=df["SAMPLE_VALUE"])
+        schema = {"model_type": name, "year": year, "model": model}
+        models_by_year[f"{name}-{year}"] = schema
+
+# Example usage:
+# model_type = "cubic_spline"
+# year = 2013
+# models_by_year[f"{model_type}-{year}"]["model"](100)
+
+# %% Create a dataframe that estimates the NDVI for each DOY (365x) and each model/year (72x)
+
+
+# cubic_spline = CubicSpline(x, y)
+# cubic_spline(100)
+# cubic_spline(150)
+
+# plt.plot(x_new, cubic_spline(x_new), "--", label="spline")
+# plt.plot(x_new, Akima1DInterpolator(x, y)(x_new), "-", label="Akima1D")
+# plt.plot(x_new, PchipInterpolator(x, y)(x_new), "-", label="pchip")
+# # plt.plot(x_new, pchip_interpolate(x, y)(x_new), '-', label='pchip2')
+# plt.plot(x, y, "o")
+# plt.legend()
+# plt.ylim(0, 1)
+# plt.show()
