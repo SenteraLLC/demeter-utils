24a8b3f150fb1c3b9f22029571b83d41ae64a24d
tnigon <8249296+tnigon@users.noreply.github.com>
2023-03-31 17:32:22 -0500

    refactor sql so it joins lat/lng at last step to be more efficient

--- demeter_utils/query/_weather.py
+++ demeter_utils/query/_weather.py
@@ -1,69 +1,13 @@
 """Python wrappers around demeter.weather SQL queries."""
-
-import warnings
 from datetime import date
-from typing import Any, List, Union
+from typing import Any, List
 
-from geopandas import GeoDataFrame, GeoSeries, read_postgis
-from pandas import DataFrame
+from demeter.weather.query import get_daily_weather_types
+from geo_utils.vector import pivot_geodataframe
+from geopandas import GeoDataFrame, read_postgis
 from psycopg2.extensions import AsIs
 from pyproj import CRS
 from shapely.geometry import Point
-
-# from geo_utils.vector import pivot_geodataframe
-
-
-def pivot_geodataframe(
-    gdf: GeoDataFrame,
-    index: Union[str, List[str]],
-    columns: Union[str, List[str]],
-    values: Union[str, List[str]] = None,
-) -> GeoDataFrame:
-    """
-    Return reshaped GeoDataFrame organized by given index / column values.
-
-    See https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.pivot.html for more information.
-    GeoDataFrame.pivot() raises a TypeError unless geometry colummn is converted to WKT. This function converts geometry
-    column to WKT, performs the pivot, then returns the result as a GeoDataFrame, setting the geometry and CRS.
-
-    Args:
-        gdf (GeoDataFrame): Inpute GeoDataFrame (in the long/tidy format).
-        index (Union[str, List[str]]): Column to use to make new frame's index. If None, uses existing index.
-        columns (Union[str, List[str]]): Column to use to make new frame's columns.
-        values (Union[str, List[str]], optional): Column(s) to use for populating new frame's values. If not specified,
-        all remaining columns will be used and the result will have hierarchically indexed columns. Defaults to None.
-
-    Returns:
-        GeoDataFrame: Reshaped GeoDataFrame (in the "wide" format).
-    """
-    geom_name = gdf.geometry.name
-    gdf_wkt = gdf.copy()
-    with warnings.catch_warnings():
-        warnings.filterwarnings(
-            action="ignore",
-            category=UserWarning,
-            message="Geometry column does not contain geometry.",
-        )
-        gdf_wkt[geom_name] = gdf_wkt[geom_name].to_wkt()  # warning is filtered
-    df_pivot = (
-        gdf_wkt.pivot(index=index, columns=columns, values=values)
-        .rename_axis(None, axis=1)
-        .reset_index()
-    )
-
-    return GeoDataFrame(
-        df_pivot, geometry=GeoSeries.from_wkt(df_pivot[geom_name]), crs=gdf.crs
-    )
-
-
-def get_daily_weather_types(cursor: Any) -> DataFrame:
-    stmt = """
-    SELECT * FROM weather.weather_type
-    WHERE temporal_extent = '1 day'
-    ORDER BY weather_type_id
-    """
-    cursor.execute(stmt)
-    return DataFrame(cursor.fetchall())
 
 
 def query_daily_weather(
@@ -88,35 +32,41 @@ def query_daily_weather(
     Returns:
         GeoDataFrame: With weather data for all coordinates and parameters between startdate and enddate (inclusive).
     """
+    # get cell_id using demeter. The query below shouldn't ever take points, it should take
+    # cursor = conn
+    # for point in coordinate_list:
+    #     get_cell_id(cursor, geometry, geometry_crs=CRS.from_epsg(4326))
+    # The SQL should only take cell_id
     stmt = """
     WITH pairs(x,y) AS (
     VALUES %(coordinate_list)s
     ), q1 AS (
-    SELECT ST_Point(x, y, 4326) as query_point, (ST_Value(raster_5km.rast_cell_id,ST_Transform(ST_Point(x, y, 4326), world_utm.raster_epsg)))::INTEGER AS cell_id
-    FROM world_utm, raster_5km
+        SELECT ST_Point(x, y, 4326) as query_point, (ST_Value(raster_5km.rast_cell_id,ST_Transform(ST_Point(x, y, 4326), world_utm.raster_epsg)))::INTEGER AS cell_id
+        FROM world_utm, raster_5km
             CROSS JOIN pairs
             WHERE ST_intersects(ST_Point(x, y, 4326), world_utm.geom) AND
             world_utm.world_utm_id=raster_5km.world_utm_id
     ), q2 AS (
-        SELECT d.cell_id, d.date_requested, d.daily_id, d.date, d.weather_type_id, d.value
+        select q1.cell_id, d.date_requested, d.daily_id, d.weather_type_id, d.date, d.value,
+            ROW_NUMBER() OVER(PARTITION BY d.cell_id, d.weather_type_id, d.date ORDER BY d.date_requested desc) as rn
         FROM daily AS d
         INNER JOIN q1
         ON q1.cell_id = d.cell_id
         where d.date >= %(startdate)s and
         d.date <= %(enddate)s and
         weather_type_id in %(weather_type_ids)s
+        GROUP BY q1.cell_id, d.date_requested, d.daily_id
     ), q3 AS (
-        SELECT q1.query_point, q1.cell_id, q2.date_requested, q2.date, q2.weather_type_id, q2.value,
-            ROW_NUMBER() OVER(PARTITION BY q1.query_point, q2.weather_type_id, q2.date ORDER BY q2.date_requested desc) as rn
-        FROM q1
-        LEFT JOIN q2
-        ON q1.cell_id = q2.cell_id
-    ) SELECT q3.cell_id, q3.date_requested, weather_type.weather_type, q3.date, q3.value, q3.query_point AS geometry
-    FROM q3
-    LEFT JOIN weather_type
-    ON q3.weather_type_id = weather_type.weather_type_id
-    WHERE q3.rn = 1
-    ORDER BY q3.query_point, q3.weather_type_id, q3.date
+        SELECT q2.cell_id, q2.date_requested, weather_type.weather_type, q2.date, q2.value, q2.weather_type_id
+        FROM q2
+        LEFT JOIN weather_type
+        ON q2.weather_type_id = weather_type.weather_type_id
+    )
+    SELECT q3.cell_id, q3.date_requested, q3.weather_type, q3.date, q3.value, q1.query_point AS geometry
+    FROM q1
+    LEFT JOIN q3
+    ON q1.cell_id = q3.cell_id
+    ORDER BY q1.query_point, q3.weather_type_id, q3.date;
     """
     assert all(
         isinstance(p, Point) for p in coordinate_list
@@ -150,6 +100,9 @@ def query_daily_weather(
         crs=CRS.from_epsg(4326),
         parse_dates=["date", "date_requested"],
     )
+
+    # After, join the query response to the input coordinate list.
+
     if wide is True:
         return pivot_geodataframe(
             gdf_sql,
--- scripts/query_weather_example.py
+++ scripts/query_weather_example.py
@@ -1,7 +1,10 @@
 # %% Imports
+from contextlib import contextmanager
 from datetime import date
+from time import perf_counter as press_button
 
 from demeter.db import getConnection
+from demeter.weather.initialize.weather_types import DAILY_WEATHER_TYPES
 from dotenv import load_dotenv
 from shapely.geometry import Point
 
@@ -13,32 +16,87 @@ c = load_dotenv()
 conn = getConnection(env_name="DEMETER-DEV_LOCAL")
 cursor = conn.connection.cursor()
 
+# %% Time context manager
+
+
+@contextmanager
+def catchtime() -> float:
+    t1 = t2 = press_button()
+    yield lambda: t2 - t1
+    t2 = press_button()
+
+
 # %% Set parameters and make query
+PARAMETERS_ALL = [t["weather_type"] for t in DAILY_WEATHER_TYPES]
 parameters = [
-    "t_min_2m_24h:C",
-    "t_max_2m_24h:C",
     "t_mean_2m_24h:C",
     "precip_24h:mm",
     "wind_speed_mean_2m_24h:ms",
-    "relative_humidity_mean_2m_24h:p",
-    "global_rad_24h:J",
 ]
+
 coordinate_list = [
     Point(-90.612269, 44.723885),
     Point(-90.612249, 44.723825),
     Point(-90.596003, 44.684680),
+    Point(-90.636620, 44.690766),
+    Point(-90.636621, 44.690766),
+    Point(-90.636622, 44.690766),
+    Point(-90.636623, 44.690766),
+    Point(-90.636624, 44.690766),
+    Point(-90.636625, 44.690766),
+    Point(-90.636626, 44.690766),
+    Point(-90.636627, 44.690766),
+    Point(-90.636628, 44.690766),
+    Point(-90.636629, 44.690766),
+    Point(-90.636620, 44.690767),
+    Point(-90.636621, 44.690767),
+    Point(-90.636622, 44.690767),
+    Point(-90.636623, 44.690767),
+    Point(-90.636624, 44.690767),
+    Point(-90.636625, 44.690767),
+    Point(-90.636626, 44.690767),
+    Point(-90.636627, 44.690767),
+    Point(-90.636628, 44.690767),
+    Point(-90.636629, 44.690767),
+    Point(-90.636620, 44.690768),
+    Point(-90.636621, 44.690768),
+    Point(-90.636622, 44.690768),
+    Point(-90.636623, 44.690768),
+    Point(-90.636624, 44.690768),
+    Point(-90.636625, 44.690768),
+    Point(-90.636626, 44.690768),
+    Point(-90.636627, 44.690768),
+    Point(-90.636628, 44.690768),
+    Point(-90.636629, 44.690768),
+    Point(-90.636620, 44.690769),
+    Point(-90.636621, 44.690769),
+    Point(-90.636622, 44.690769),
+    Point(-90.636623, 44.690769),
+    Point(-90.636624, 44.690769),
+    Point(-90.636625, 44.690769),
+    Point(-90.636626, 44.690769),
+    Point(-90.636627, 44.690769),
+    Point(-90.636628, 44.690769),
+    Point(-90.636629, 44.690769),
+    Point(-90.63662, 44.690766),
+    Point(-90.636626, 44.690766),
+    Point(-90.636626, 44.690766),
     Point(-90.636626, 44.690766),
 ]
 startdate = date(2013, 1, 1)
 enddate = date(2023, 3, 31)
 
-gdf_sql = query_daily_weather(
-    conn,
-    coordinate_list,
-    startdate,
-    enddate,
-    parameters,
-    wide=False,
-)
 
-# %%
+with catchtime() as t:
+    gdf_sql = query_daily_weather(
+        conn=conn,
+        coordinate_list=coordinate_list,
+        startdate=startdate,
+        enddate=enddate,
+        parameters=PARAMETERS_ALL,
+        wide=False,
+    )
+print(f"query_daily_weather() time: {t():.1f} seconds")
+# %% Timing
+# 1.958 M rows (47 coords (3 unique cell_ids) x 10 years x 11 params): 101.5 seconds
+#
