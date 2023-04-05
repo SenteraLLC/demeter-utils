943c4f1ef8ae5a2be092471d989b1912f3a9dc85
Deepak Ghimire <deepak.ghimire@sentera.com>
2023-04-05 15:38:13 -0500

    updating interpolated moduele

--- demeter_utils/interpolate/_interpolate.py
+++ demeter_utils/interpolate/_interpolate.py
@@ -1,54 +1,68 @@
+from datetime import datetime
 from typing import Union
 
+import numpy as np
 from pandas import DataFrame
+from pandas import concat as pd_concat
 from scipy.interpolate import Akima1DInterpolator, CubicSpline, PchipInterpolator
 
+# from pandas import merge as pd_merge
+# from pandas import read_csv
+#    """"
 # Step 1: Load data that already exists (that may or may not represent an entire growing season)
 
+#     The dataset should have at least 'doy' and 'value_observed' columns
+#    """
 
-def generate_fill_in_values_a(
-    df_true_data: DataFrame, temporal_resolution: int, starttime: int, endtime: int
+
+# %% Functions
+def find_fill_in_dates(
+    df_true_data: DataFrame, starttime: int, endtime: int, temporal_resolution: int
 ) -> DataFrame:
     """
     # Step 2a: Given an argument `temporal_resolution`, determine which times/days are absent from input data (step 1)
 
     Args:
         df_true_data (`DataFrame`): Input ("true") data.
-        temporal_resolution (`int`): The minimum temporal resolution of the combined "true" and "fill-in" data.
         starttime (`int`): The relative starting date for which data are required.
         endtime (`int`): The relative end date for which data are required.
+        temporal_resolution (`int`): The minimum temporal resolution of the combined "true" and "fill-in" data.
 
     Returns:
         `DataFrame`: Whether "true" data are available for a given temporal resolution and date range (True or False).
     """
-    print("Hello A")
-    df_inventory = DataFrame()
+    df_in = df_true_data
+
+    date_plant = datetime(2022, 5, 1)
+    df_in["dap"] = (df_in["date_observed"] - date_plant).dt.days
+
+    df_join = DataFrame(data=[], columns=df_in.columns)
+    df_join["dap"] = np.arange(
+        np.timedelta64(starttime, "D"),
+        np.timedelta64(endtime, "D"),
+        np.timedelta64(temporal_resolution, "D"),
+    ).astype(np.timedelta64)
+    df_join["dap"] = df_join["dap"].dt.days
+
+    df_inventory = (
+        pd_concat([df_in, df_join])
+        .drop_duplicates(subset=["dap"], keep="first")
+        .reset_index(drop=True)
+    )
+
+    available = []
+    for i in df_inventory["value_observed"]:
+        if i <= 1:
+            available.append("True")
+        else:
+            available.append("False")
+
+    df_inventory["available"] = available
 
     return df_inventory
 
-    # Example:
-    #     True data:
-    #         DAP     value
-    #         20      0.4
-    #         30      0.5
-    #         40      0.6
 
-    #     Desired output for:
-    #         `generate_fill_in_values_a(df_true_data, temporal_resolution=10, starttime=0, endtime=100)
-    #             day     available   value
-    #             0       False       NA
-    #             10      False       NA
-    #             20      True        0.4
-    #             30      True        0.5
-    #             40      True        0.6
-    #             50      False       NA
-    #             60      False
-    #             70      False
-    #             80      False
-    #             90      False
-    #             100     False
-
-
+# %%
 def generate_fill_in_values_b(
     df_inventory: DataFrame,
     interp_func: Union[Akima1DInterpolator, CubicSpline, PchipInterpolator],
@@ -59,6 +73,9 @@ def generate_fill_in_values_b(
     Returns:
         `DataFrame`: Keeps "true" data alone, but interpolates for all required "fill-in" dates.
     """
+
+    # df["value"] = df["value"].fillna(0)
+
     print("Hello B")
 
 
