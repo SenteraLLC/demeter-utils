a4c4f7c1afa57ca428601b7c4dd871e25ad7e9ec
Deepak Ghimire <deepak.ghimire@sentera.com>
2023-04-17 14:16:55 -0500

    cleaning scripts for review

--- demeter_utils/interpolate/_interpolate.py
+++ demeter_utils/interpolate/_interpolate.py
@@ -1,142 +1,166 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
 from pandas import concat as pd_concat
 
+# def find_fill_in_dates(
+#     df_true_data: DataFrame,
+#     starttime: int,
+#     endtime: int,
+#     temporal_resolution: int,
+#     date_plant: datetime,
+# ) -> DataFrame:
+#     """Determine which 'days of year' of 'day after planting' are absent in the input data
+#        The input data should have at least `date_observed` and `value_observed` column
 
-def find_fill_in_dates(
-    df_true_data: DataFrame,
-    starttime: int,
-    endtime: int,
-    temporal_resolution: int,
-    date_plant: datetime,
-) -> DataFrame:
-    """Determine which 'days of year' of 'day after planting' are absent in the input data
-       The input data should have at least `date_observed` and `value_observed` column
+#     Args:
+#         df_true_data (`DataFrame`): Input ("true") data that is available for a season/year.
+#         starttime (`int`): The relative starting date for which data are required.
+#         endtime (`int`): The relative end date for which data are required.
+#         temporal_resolution (`int`): The minimum temporal resolution desired for the output data.
+#         date_plant (`datetime`): The date of planting, if available. Otherwise first day of year which will return "doy" instead of "dap"
 
-    Args:
-        df_true_data (`DataFrame`): Input ("true") data that is available for a season/year.
-        starttime (`int`): The relative starting date for which data are required.
-        endtime (`int`): The relative end date for which data are required.
-        temporal_resolution (`int`): The minimum temporal resolution desired for the output data.
-        date_plant (`datetime`): The date of planting, if available. Otherwise first day of year which will return "doy" instead of "dap"
+#     Returns:
+#         `DataFrame`: input data concat with a new empty data, column `available` will indicate whether "true" data are
+#         available for a given temporal resolution and date range (if avaialbe `True`, else `False`).
+#     """
+#     # rename the data frame
+#     df_in = df_true_data
 
-    Returns:
-        `DataFrame`: input data concat with a new empty data, column `available` will indicate whether "true" data are
-        available for a given temporal resolution and date range (if avaialbe `True`, else `False`).
-    """
-    # rename the data frame
-    df_in = df_true_data
+#     # create a new column `dap` for day after planting in data `df_in`
+#     df_in["dap"] = (df_in["date_observed"] - date_plant).dt.days
 
-    # create a new column `dap` for day after planting in data `df_in`
-    df_in["dap"] = (df_in["date_observed"] - date_plant).dt.days
+#     # create a empty dataframe with all columns in `df_in` dataframe and add a column `dap` based on user input `starttime`, `endtime` and `temporal_resolution`
+#     df_join = DataFrame(data=[], columns=df_in.columns)
+#     df_join["dap"] = np.arange(
+#         np.timedelta64(starttime, "D"),
+#         np.timedelta64(endtime, "D"),
+#         np.timedelta64(temporal_resolution, "D"),
+#     ).astype(np.timedelta64)
+#     df_join["dap"] = df_join["dap"].dt.days
 
-    # create a empty dataframe with all columns in `df_in` dataframe and add a column `dap` based on user input `starttime`, `endtime` and `temporal_resolution`
-    df_join = DataFrame(data=[], columns=df_in.columns)
-    df_join["dap"] = np.arange(
-        np.timedelta64(starttime, "D"),
-        np.timedelta64(endtime, "D"),
-        np.timedelta64(temporal_resolution, "D"),
-    ).astype(np.timedelta64)
-    df_join["dap"] = df_join["dap"].dt.days
+#     # concat two dataframes `df_in` and `df_join`; if `dap` values in two dataframe is duplicate, keep the one from `df_in` only
+#     # because `df_in` has `true` values
+#     df_observed = (
+#         pd_concat([df_in, df_join])
+#         .drop_duplicates(subset=["dap"], keep="first")
+#         .reset_index(drop=True)
+#     )
 
-    # concat two dataframes `df_in` and `df_join`; if `dap` values in two dataframe is duplicate, keep the one from `df_in` only
-    # because `df_in` has `true` values
-    df_observed = (
-        pd_concat([df_in, df_join])
-        .drop_duplicates(subset=["dap"], keep="first")
-        .reset_index(drop=True)
-    )
+#     # create a new column `doy_obs` by extract the day of year from `dap` column
+#     df_observed["doy_obs"] = df_observed["dap"] + date_plant.timetuple().tm_yday
 
-    # create a new column `doy_obs` by extract the day of year from `dap` column
-    df_observed["doy_obs"] = df_observed["dap"] + date_plant.timetuple().tm_yday
+#     # add new column `available` to `df_observed` where true or false is returned based the condition, 'value_observed <=1'
+#     available = []
+#     for i in df_observed["value_observed"]:
+#         if i <= 1:
+#             available.append("True")
+#         else:
+#             available.append("False")
 
-    # add new column `available` to `df_observed` where true or false is returned based the condition, 'value_observed <=1'
-    available = []
-    for i in df_observed["value_observed"]:
-        if i <= 1:
-            available.append("True")
-        else:
-            available.append("False")
+#     df_observed["available"] = available
 
-    df_observed["available"] = available
+#     # sort the dataframe by `doy_obs` in ascending order
+#     df_observed = df_observed.sort_values(by=["doy_obs"])
 
-    # sort the dataframe by `doy_obs` in ascending order
-    df_observed = df_observed.sort_values(by=["doy_obs"])
-
-    return df_observed
-
-
-# %% Example use:
-
-# TODO: read data from cloud
-# df_true_data = read_csv(
-#     "/root/git/demeter-utils/df_drone_imagery1.csv",
-#     parse_dates=["date_observed", "last_updated"],
-# )
-# df_observed = find_fill_in_dates(df_true_data, 0, 70, 10, datetime(2022, 4, 15))
+#     return df_observed
 
 
-# %% Example:
-#     True data:
-#               date_observed         value_observed
-#               2022-05-01             0.4
-#               2022-05-15             0.5
-#               2022-05-25             0.6
+# # %% Example use:
 
-#     Desired output for:
-#         find_fill_in_dates(df_true_data, starttime=0, endtime=70, temporal_resolution=10, plant_date=datetime(2022,4,15))
-#
-#               date_observed         value_observed        dap    doy_obs     available
-#                 NA                    NA                   0       105         False
-#                 NA                    NA                   10      115         False
-#               2022-05-01              0.4                  16      121         True
-#               2022-05-15              0.5                  20      125         True
-#               2022-05-25              0.6                  30      135         True
-#                 NA                    NA                   40      145         False
-#                 NA                    NA                   50      155         False
-#                 NA                    NA                   60      165         False
+# # TODO: read data from cloud
+# # df_true_data = read_csv(
+# #     "/root/git/demeter-utils/df_drone_imagery1.csv",
+# #     parse_dates=["date_observed", "last_updated"],
+# # )
+# # df_observed = find_fill_in_dates(df_true_data, 0, 70, 10, datetime(2022, 4, 15))
 
 
+# # %% Example:
+# #     True data:
+# #               date_observed         value_observed
+# #               2022-05-01             0.4
+# #               2022-05-15             0.5
+# #               2022-05-25             0.6
+
+# #     Desired output for:
+# #         find_fill_in_dates(df_true_data, starttime=0, endtime=70, temporal_resolution=10, plant_date=datetime(2022,4,15))
+# #
+# #               date_observed         value_observed        dap    doy_obs     available
+# #                 NA                    NA                   0       105         False
+# #                 NA                    NA                   10      115         False
+# #               2022-05-01              0.4                  16      121         True
+# #               2022-05-15              0.5                  20      125         True
+# #               2022-05-25              0.6                  30      135         True
+# #                 NA                    NA                   40      145         False
+# #                 NA                    NA                   50      155         False
+# #                 NA                    NA                   60      165         False
+
+
+# %%
 def generate_fill_in_values(
     df_reference_ndvi: DataFrame,
+    datetime_start: datetime,
+    datetime_end: datetime,
+    temporal_resolution: timedelta,
     interp_function: str,
 ) -> DataFrame:
     """
     # Generate a dataframe with interpolated values given a standard/reference data `df_reference_ndvi' and `interp_function`
-     The input dataset should have at least `START DATE` and `SAMPLE VALUE` columns.
+     The input dataset should have at least `date_start` and `sample_value` columns.
 
     Args:
         df_reference_ndvi (`DataFrame`): Input ("reference") data.
+        datetime_start (`datetime.datetime`): Starting datetime for interpolation of reference data.
+        datetime_end (`datetime.datetime`): Ending datetime for interpolation of reference data.
+        temporal_resolution (`datetime.timedelta`): The minimum temporal resolution desired for the output interpolation of reference data.
         interp_function (`str`): Model type for interpolation, "CubicSpline" for cubic spline, "Akima1DInterpolator" for akima1DInterpolator, "PchipInterpolator" for pchip_interpolator
 
-    Returns:
-        `DataFrame`: Input dataset plus three added columns; 'model_type', 'doy_interp' for interpolated temporal resolution and 'ndvi_interp' for interpolated ndvi values.
+    Returns `DataFrame` wtih following columns:
+        "datetime_interp": Datetime for the value observed/interpolated
+        "doy_interp": Day of the year for the corresponding `datetime_interp`
+        "model_type": Interpolation model/function used
+        "ndvi_interp": Value of NDVI observed/interpolated
+
     """
 
-    # create an arrary of day of year `doy_interp` values for interpolation
-    doy_interp = np.arange(0, 365, 1).astype(int)
+    # copy the `df_reference_ndvi` as `df_forinterp`
+    df_forinterp = df_reference_ndvi.copy()
 
-    # Remove NA values from the 'sample value' column in the data 'df_reference_ndvi'
-    df_reference_ndvi = df_reference_ndvi[df_reference_ndvi["SAMPLE VALUE"].notna()]
+    # Remove NA values from the 'sample_value' column in the data 'df_forinterp'
+    df_forinterp = df_forinterp[df_forinterp["sample_value"].notna()]
 
-    # TODO: For broader use of thie function, define condition to turn the below two chunks only if `doy_obs` column
-    # is not present in the dataset
+    # Convert the 'date_start' column to a datetime.datetime() object
+    df_forinterp["date_start"] = (df_forinterp["date_start"]).astype(np.datetime64)
 
-    # Convert the 'start date' column to a datetime object
-    df_reference_ndvi["START DATE"] = (df_reference_ndvi["START DATE"]).astype(
-        np.datetime64
-    )
-
-    # Extract the day of year from the 'start date' column and store it in a new column 'doy_obs'
-    df_reference_ndvi["doy_obs"] = df_reference_ndvi["START DATE"].apply(
+    # Extract the day of year from the 'date_start' column and store it in a new column 'doy_obs'
+    df_forinterp["doy_obs"] = df_forinterp["date_start"].apply(
         lambda x: x.timetuple().tm_yday
     )
 
-    # remane data frame to 'df_forinterp'
-    df_forinterp = df_reference_ndvi
+    # create an arrary of `datetime_interp` values for interpolation
+    datetime_interp = np.arange(datetime_start, datetime_end, temporal_resolution)
+
+    # create a temp df `df_forinterp_temp` to store `datetime_interp` and `doy_interp`
+    df_forinterp_temp = pd.DataFrame(
+        datetime_interp, columns=["datetime_interp"]
+    ).astype(np.datetime64)
+
+    # convert the `datetime_interp` to `doy_interp` and add the values to a new column
+    df_forinterp_temp["doy_interp"] = df_forinterp_temp["datetime_interp"].apply(
+        lambda x: x.timetuple().tm_yday
+    )
+
+    doy_interp = df_forinterp_temp["doy_interp"]
+
+    # %%
+
+    # assign `x_interp`, `x_obs` and `y_obs` values
+    x_interp = doy_interp  # Or, datetime_interp: The code does not work for datetime_interp as of now
+    x_obs = df_forinterp["doy_obs"]
+    y_obs = df_forinterp["sample_value"]
 
     # create a new data frame 'df_reference_interp' to store the interpolated values.
     df_reference_interp = pd.DataFrame(
@@ -144,37 +168,58 @@ def generate_fill_in_values(
     )
 
     # generate interpolated ndvi values using the interp_function specified in function
-    ndvi_interp = interp_function(
-        x=df_forinterp["doy_obs"], y=df_forinterp["SAMPLE VALUE"]
-    )(doy_interp)
+    ndvi_interp = interp_function(x=x_obs, y=y_obs)(x_interp)
 
     data = {
-        "model_type": interp_function,
-        "doy_interp": doy_interp,
+        "model_type": str(interp_function),
+        "doy_interp": x_interp,
         "ndvi_interp": ndvi_interp,
     }
     df_temp = pd.DataFrame(data=data)
-    df_reference_interp = pd.concat([df_reference_interp, df_temp], axis=0)
+
+    # create a df `df_reference_interp` with concat of `df_reference_interp` and `df_temp`
+    df_reference_interp = pd_concat([df_reference_interp, df_temp], axis=0)
+
+    # create a df `df_reference_interp_merged` by merging `df_reference_interp` and `df_forinterp_temp` based on 'doy_interp' column
+    df_reference_interp_merged = pd.merge(
+        df_forinterp_temp, df_reference_interp, on="doy_interp"
+    )
 
     # remove NA values from the dataframe
-    df_reference_interp = df_reference_interp[
-        df_reference_interp["ndvi_interp"].notna()
+    df_reference_interp_merged = df_reference_interp_merged[
+        df_reference_interp_merged["ndvi_interp"].notna()
     ]
-    return df_reference_interp
+    return df_reference_interp_merged
 
 
 # %% Example use:
 
-# import the data from gimms portal
+# load the standard/reference data
 # TODO: Use the function by Marissa
+# GIMMS_COLS = {
+#     "START DATE": "date_start",
+#     "END DATE": "date_end",
+#     "SAMPLE VALUE": "sample_value",
+#     "SAMPLE COUNT": "n_pixels_sample",
+#     "MEAN VALUE": "mean_hist_value",
+#     "MIN VALUE": "min_hist_value",
+#     "MAX VALUE": "max_hist_value",
+# }
 # req = requests.get(
 #     "https://glam1.gsfc.nasa.gov/api/gettbl/v4?sat=MOD&version=v11&layer=NDVI&mask=NASS_2011-2016_corn&shape=ADM&ids=110955&ts_type=seasonal&years=2019&start_month=1&num_months=12&format=csv"
 # )
 # text = StringIO(req.content.decode("utf-8"))
-# df_gimms_ndvi = read_csv(text, skiprows=14)
+# df_gimms_ndvi = read_csv(text, skiprows=14).rename(columns=GIMMS_COLS)[
+#     GIMMS_COLS.values()
+# ]
 
-# df_interp = generate_fill_in_values(df_gimms_ndvi, Akima1DInterpolator)
-
+# df_interp = generate_fill_in_values(
+#     df_reference_ndvi = df_gimms_ndvi,
+#     datetime_start = datetime(2019, 5, 6),
+#     datetime_end = datetime(2019, 10, 1),
+#     temporal_resolution = timedelta(days=1),
+#     interp_function = CubicSpline,
+#     )
 
 # %% Example:
 #     True data:
@@ -184,8 +229,49 @@ def generate_fill_in_values(
 #               2022-05-25             0.6
 
 #     Desired output for:
-#         generate_fill_in_values(df_reference_ndvi = df_gimms_ndvi, interp_function = Akima1DInterpolator))
+#         df_interp = generate_fill_in_values(
+#     df_reference_ndvi = df_gimms_ndvi,
+#     datetime_start = datetime(2019, 5, 6),
+#     datetime_end = datetime(2019, 10, 1),
+#     temporal_resolution = timedelta(days=1),
+#     interp_function = CubicSpline,
+#     )
 #
 #               model_type         doy_interp        ndvi_interp
 #           Akima1DInterpolator         1               0.265
 #           Akima1DInterpolator         2               0.275
+
+
+# %% TODO: Delete if `doy_interp` is used instead of `datetime_interp`
+# convert `doy_interp` to float
+# def dt64_to_float(dt64):
+#     """Converts numpy.datetime64 to year as float.
+
+#     Rounded to days
+
+#     Parameters
+#     ----------
+#     dt64 : np.datetime64 or np.ndarray(dtype='datetime64[X]')
+#         date data
+
+#     Returns
+#     -------
+#     float or np.ndarray(dtype=float)
+#         Year in floating point representation
+#     """
+
+#     year = dt64.astype('M8[Y]')
+#     days = (dt64 - year).astype('timedelta64[D]')
+#     year_next = year + np.timedelta64(1, 'Y')
+#     days_of_year = (year_next.astype('M8[D]') - year.astype('M8[D]')
+#                     ).astype('timedelta64[D]')
+#     dt_float = 1970 + year.astype(float) + days / (days_of_year)
+#     return dt_float
+
+# doy_interp_float = dt64_to_float(doy_interp)
+
+
+# TODO: Delete this if not required
+# datetime_obs_array = df_forinterp["date_start"].to_numpy()
+# datetime_obs_float = dt64_to_float(datetime_obs_array)
+# %%
--- scripts/interpolate_example.py
+++ scripts/interpolate_example.py
@@ -1,35 +1,39 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from io import StringIO
 
 import requests
 from pandas import read_csv
 from scipy.interpolate import Akima1DInterpolator
 
-from demeter_utils.interpolate._interpolate import (
-    find_fill_in_dates,
-    generate_fill_in_values,
-)
+from demeter_utils.interpolate._interpolate import generate_fill_in_values
 
 # from scipy.interpolate import Akima1DInterpolator, CubicSpline, PchipInterpolator
 
-# load the true data
-# TODO: read data from cloud
-# data available at sharepoint:https://sentera.sharepoint.com/:x:/s/demeter/ESR0PKnkjQBIkYDkT9NBVS8B1h5kJHTbJE2tCLgM7QWP7A?e=ioUoFB
-df_true_data = read_csv(
-    "/root/git/demeter-utils/df_drone_imagery1.csv",
-    parse_dates=["date_observed", "last_updated"],
-)
-
-# using `find_fill_in_dates function` to generate df_observed
-df_observed = find_fill_in_dates(df_true_data, 0, 120, 10, datetime(2022, 5, 6))
-
 # load the standard/reference data
 # TODO: Use the function by Marissa
+GIMMS_COLS = {
+    "START DATE": "date_start",
+    "END DATE": "date_end",
+    "SAMPLE VALUE": "sample_value",
+    "SAMPLE COUNT": "n_pixels_sample",
+    "MEAN VALUE": "mean_hist_value",
+    "MIN VALUE": "min_hist_value",
+    "MAX VALUE": "max_hist_value",
+}
 req = requests.get(
     "https://glam1.gsfc.nasa.gov/api/gettbl/v4?sat=MOD&version=v11&layer=NDVI&mask=NASS_2011-2016_corn&shape=ADM&ids=110955&ts_type=seasonal&years=2019&start_month=1&num_months=12&format=csv"
 )
 text = StringIO(req.content.decode("utf-8"))
-df_gimms_ndvi = read_csv(text, skiprows=14)
+df_gimms_ndvi = read_csv(text, skiprows=14).rename(columns=GIMMS_COLS)[
+    GIMMS_COLS.values()
+]
+
 
 # using `generate_fill_in_values` to generate df_interp
-df_reference_interp = generate_fill_in_values(df_gimms_ndvi, Akima1DInterpolator)
+df_reference_interp = df_interp = generate_fill_in_values(
+    df_reference_ndvi=df_gimms_ndvi,
+    datetime_start=datetime(2019, 5, 6),
+    datetime_end=datetime(2019, 10, 1),
+    temporal_resolution=timedelta(days=1),
+    interp_function=Akima1DInterpolator,
+)
