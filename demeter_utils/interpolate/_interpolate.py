461acf99eb990141e194690925a0a826903f4625
Deepak Ghimire <deepak.ghimire@sentera.com>
2023-04-20 12:27:48 -0500

    updating get_inference_fx_from_df_reference function

--- demeter_utils/interpolate/_interpolate.py
+++ demeter_utils/interpolate/_interpolate.py
@@ -237,6 +237,8 @@ def get_datetime_skeleton_for_ts(
 def get_inference_fx_from_df_reference(
     df_reference: DataFrame,
     interp_type: str,
+    col_datetime: str = "date_start",
+    col_value: str = "sample_value",
 ) -> Callable:
     """
     # Generate a inference function from a reference data based on interpolation type
@@ -244,6 +246,8 @@ def get_inference_fx_from_df_reference(
     Args:
         df_reference (`DataFrame`): Standard ("reference") data.
         interp_type (`str`): Model type for interpolation, "CubicSpline" for cubic spline, "Akima1DInterpolator" for akima1DInterpolator, "PchipInterpolator" for pchip_interpolator
+        col_datetime (`str`): Column name for column in `df_reference` that holds time series temporal data.
+        col_value (`str`): Column name for column in `df_reference` that holds time series value data.
 
     Returns the fitted interpolation function
     """
@@ -252,20 +256,19 @@ def get_inference_fx_from_df_reference(
     df_forinterp = df_reference.copy()
 
     # Remove row with NA values in the 'sample_value' column from the data 'df_forinterp'
-    df_forinterp = df_forinterp[df_forinterp["sample_value"].notna()]
+    df_forinterp = df_forinterp[df_forinterp[col_value].notna()]
 
     # Convert the 'date_start' column to a datetime.datetime() object
-    df_forinterp["date_start"] = (df_forinterp["date_start"]).astype(datetime64)
+    df_forinterp[col_datetime] = (df_forinterp[col_datetime]).astype(datetime64)
 
     # assign `x_obs` and `y_obs` values
-    x_obs = df_forinterp["date_start"]
-    y_obs = df_forinterp["sample_value"]
+    x_obs = df_forinterp[col_datetime]
+    y_obs = df_forinterp[col_value]
 
-    # create the `infer_fucntion` function
-    def infer_function(x_interp):
-        return interp_type(x=x_obs, y=y_obs)(x_interp)
+    # define the function
+    fx = interp_type(x=x_obs, y=y_obs)
 
-    return infer_function
+    return fx
 
 
 def populate_fill_in_values(
