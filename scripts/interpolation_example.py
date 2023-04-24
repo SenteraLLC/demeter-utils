a1813bd5e5b6a7adaf6bea7b4fcd65eb916b8cd6
Deepak Ghimire <deepak.ghimire@sentera.com>
2023-04-24 13:07:42 -0500

    cleaning interpolation_example.py

--- scripts/interpolation_example.py
+++ scripts/interpolation_example.py
@@ -4,6 +4,7 @@ from io import StringIO
 
 import matplotlib.pyplot as plt
 import requests
+from numpy import datetime64
 from pandas import DataFrame, Series, read_csv, to_numeric
 from scipy.interpolate import Akima1DInterpolator, CubicSpline, PchipInterpolator
 
@@ -12,6 +13,141 @@ from demeter_utils.temporal_inference import (
     populate_fill_in_values,
     train_inference_from_df,
 )
+
+# %% Testing on real data
+# link to data: https://sentera.sharepoint.com/:x:/s/demeter/ESR0PKnkjQBIkYDkT9NBVS8B1h5kJHTbJE2tCLgM7QWP7A?e=Vtcdlw
+
+# %%
+# load/generate a `as-available` dataframe
+df_true = read_csv("df_drone_imagery1.csv")
+
+# TODO: Should we do this in the function itself??
+# Convert the 'date_start' column to a datetime.datetime() object
+df_true["date_observed"] = (df_true["date_observed"]).astype(datetime64)
+
+# generate a skeleton dataframe with `sample_values` for each datetime where available and NA where `sample_values` are unavailable
+df_skeleton = get_datetime_skeleton_for_ts(
+    df_true_data=df_true,
+    datetime_start=datetime(2022, 3, 1),
+    datetime_end=datetime(2022, 10, 31),
+    temporal_resolution=timedelta(days=7),
+    tolerance_alpha=0.5,
+    col_datetime="date_observed",
+    col_value="value_observed",
+    recalibrate=True,
+)
+
+# %%
+# load the standard/reference data
+# TODO: Use the function by Marissa
+GIMMS_COLS = {
+    "START DATE": "date_start",
+    "END DATE": "date_end",
+    "SAMPLE VALUE": "sample_value",
+    "SAMPLE COUNT": "n_pixels_sample",
+    "MEAN VALUE": "mean_hist_value",
+    "MIN VALUE": "min_hist_value",
+    "MAX VALUE": "max_hist_value",
+}
+req = requests.get(
+    "https://glam1.gsfc.nasa.gov/api/gettbl/v4?sat=MOD&version=v11&layer=NDVI&mask=NASS_2011-2016_corn&shape=ADM&ids=110955&ts_type=seasonal&years=2022&start_month=1&num_months=12&format=csv"
+)
+text = StringIO(req.content.decode("utf-8"))
+df_gimms_ndvi = read_csv(text, skiprows=14).rename(columns=GIMMS_COLS)[
+    GIMMS_COLS.values()
+]
+
+# %%
+# generate the infer_function
+infer_function = train_inference_from_df(
+    df_reference=df_gimms_ndvi,
+    interp_type=PchipInterpolator,
+)
+
+# %%
+# generate dataframe where NA values are replaced with values obtained using the `infer_function`
+df_skeleton_final = populate_fill_in_values(
+    df_skeleton=df_skeleton,
+    infer_function=infer_function,
+    col_datetime="datetime_skeleton",
+    col_value="value_observed",
+)
+
+# %%
+# generate an `interp_function` based on `df_skeleton_final` obtained from step above
+interp_function = train_inference_from_df(
+    df_reference=df_skeleton_final,
+    interp_type=PchipInterpolator,
+    col_datetime="datetime_skeleton",
+    col_value="value_observed",
+)
+
+# interpolate the values in `df_skeleton_final` using `interp_function` defined in above step
+# obtain interpolated value for a single datetime
+# interp_function(to_numeric(Series([datetime(2022, 10, 31)])))[0]
+
+# %%
+# to obtain interpolated value for a complete df
+df_skeleton_new = get_datetime_skeleton_for_ts(
+    df_true_data=df_skeleton_final,
+    datetime_start=datetime(2022, 3, 1),
+    datetime_end=datetime(2022, 10, 31),
+    temporal_resolution=timedelta(days=1),  # temporal resolution of 1 day
+    tolerance_alpha=0.5,
+    col_datetime="datetime_skeleton",
+    col_value="value_observed",
+    recalibrate=True,
+)
+
+# %% Testing final interpolation with different interpolation function
+
+interp_type = [Akima1DInterpolator, CubicSpline, PchipInterpolator]
+
+interp_functions = []
+for i in interp_type:
+    interp_function = train_inference_from_df(
+        df_reference=df_skeleton_final,
+        interp_type=i,
+        col_datetime="datetime_skeleton",
+        col_value="value_observed",
+    )
+    interp_functions.append(interp_function)
+
+dfs_interpolated = []
+for i in interp_functions:
+    df_interpolated = populate_fill_in_values(
+        df_skeleton=df_skeleton_new,
+        infer_function=i,
+        col_datetime="datetime_skeleton",
+        col_value="value_observed",
+    )
+    dfs_interpolated.append(df_interpolated)
+
+df_interpolated_akima = dfs_interpolated[0]
+df_interpolated_cubicspline = dfs_interpolated[1]
+df_interpolated_pchip = dfs_interpolated[2]
+
+
+# %% Plots
+datetime_interp = df_interpolated_akima["datetime_skeleton"]
+
+plt.plot(datetime_interp, df_interpolated_akima["value_observed"], "-", label="Akima1D")
+plt.plot(
+    datetime_interp, df_interpolated_cubicspline["value_observed"], "--", label="spline"
+)
+plt.plot(datetime_interp, df_interpolated_pchip["value_observed"], "-.", label="pchip1")
+plt.scatter(df_true["date_observed"], df_true["value_observed"])
+plt.legend()
+plt.title("Interpolated Graph")
+plt.ylim(0, 1.2)
+plt.xticks(rotation=60)
+plt.show()
+
+
+# %%
+#######################################################
+#### TESTING ON HYPOTHETICAL DATA & CAN BE DELETED
+#######################################################
 
 # NOTE: Steps 0, 1, and 2 are same as in `fill_in_example.py`
 # %% Step 0: Generate dataframe from dem-357
