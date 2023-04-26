650b86ea6844e7e11b6c04a0bd43e0e35900d9ee
Deepak Ghimire <deepak.ghimire@sentera.com>
2023-04-26 10:05:57 -0500

    adding dbl_sigmoid_functions

--- demeter_utils/temporal_inference/_train.py
+++ demeter_utils/temporal_inference/_train.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from typing import Callable
 
-from numpy import datetime64
+from numpy import around, array, datetime64, exp, percentile
 from pandas import DataFrame
+from scipy.optimize import curve_fit
 
 
 def train_inference_from_df(
@@ -41,3 +43,106 @@ def train_inference_from_df(
     fx = interp_type(x=x_obs, y=y_obs)
 
     return fx
+
+
+# double sigmoid function [CASE 1: Generate y_pred based on input model parameters]]
+def dbl_sigmoid_function_1(params: list, t: datetime):
+    """The double sigmoid function defined over t (where t is an array).
+    params = list of 6 parameters
+    where, p0 = value_observed at planting/emergence,
+    p1 = max value_observed at peak growing season
+    p2 = rate of increase of value_observed at p3
+    p3 = x-value (doy_obs) for which p2 is calculated
+    p4 = rate of decrease of value_observed at p5
+    p5 = x-value (doy_obs) for which p4 is calcualted
+
+    t = array of datetime #TODO: Curently `doy` is used instead of `datetime`. Need to make it work with datetime
+
+    Returns an array of predicted/fitted values
+    """
+
+    sigma1 = 1.0 / (1 + exp(params[2] * (t - params[3])))
+    sigma2 = 1.0 / (1 + exp(-params[4] * (t - params[5])))
+    y = params[0] - params[1] * (sigma1 + sigma2 - 1)
+    return y
+
+
+# double sigmoid function [Case 2: Generate model parameters from given standard data and plug in those parameters to generate a refined function]
+def dbl_sigmoid_function_2(
+    df=DataFrame,
+    col_datetime=str,
+    col_value=str,
+    guess=None,
+) -> Callable:
+    """
+    Perform double logistic regression of `col_value` with respect to `col_datetime` from given dataframe and retun a fitted function.
+
+    Parameters:
+    df (`DataFrame): A dataframe with at least two columns `col_datetime` and `col_value`
+    col_datetime (`str`): Column name for column in `df` that holds time series temporal data.
+    col_value (`str`): Column name for column in `df` that holds time series value data.
+
+    guess (dict): dictionary of initial parameter values (optional)
+    where,
+    p0 = value_observed at planting/emergence [min value on left tail]
+    p1 = max value_observed at peak growing season
+    p2 = rate of increase of value_observed at p3
+    p3 = x-value (doy_obs) for which p2 is calculated
+    p4 = rate of decrease of value_observed at p5
+    p5 = x-value (doy_obs) for which p4 is calcualted
+
+    Returns:
+    A fitted double logistic regression function
+    """
+    # TODO: The col_datetime currently only takes 'DOY' values. Some work is needed to make the code work to take `datetime` as input
+
+    # define x and y for the input dataframe
+    x = df[col_datetime].astype(float)
+    y = df[col_value]
+
+    # Define the double logistic function
+    def func(x, p0, p1, p2, p3, p4, p5):
+        y = p0 - p1 * (
+            (1.0 / (1 + exp(p2 * (x - p3)))) + (1.0 / (1 + exp(-p4 * (x - p5)))) - 1
+        )
+        return y
+
+    # If no initial parameter values are specified, make an educated guess based on the data
+    if guess is None:
+        guess = {
+            "p0": min(y),
+            "p1": max(y),
+            "p2": 0.035,
+            "p3": percentile(x, 25),
+            "p4": 0.035,
+            "p5": percentile(x, 75),
+        }
+
+    guess = [*guess.values()]
+
+    print("guess paramters:", list(around(array(guess), 4)))
+
+    # Fit the curve to the data
+    popt, pcov = curve_fit(
+        func,
+        xdata=x,
+        ydata=y,
+        p0=[guess],
+    )
+
+    # return an array of the fitted parameters
+    popt
+    print("refined paramters:", list(around(array(popt), 4)))
+
+    # Refine the double sigmoid function based on the above curve fitting
+    def fitted_dbl_sigmoid_func(x):
+        p = popt
+        y = p[0] - p[1] * (
+            (1.0 / (1 + exp(p[2] * (x - p[3]))))
+            + (1.0 / (1 + exp(-p[4] * (x - p[5]))))
+            - 1
+        )
+        return y
+
+    # return the refined function
+    return fitted_dbl_sigmoid_func
