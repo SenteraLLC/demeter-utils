091c5603856499291bfbff3ffa9edb1a9be4c5ff
tnigon <8249296+tnigon@users.noreply.github.com>
2023-10-04 13:43:57 -0500

    adapt data_ingest from FIRST seed tests

--- demeter_utils/data_ingest/__main__.py
+++ demeter_utils/data_ingest/__main__.py
@@ -1,66 +1,129 @@
-"""Script to gather and insert Bayer Canola Phase 1 data into the `demeter` database.
+"""
+Script to load and organize Customer data from Odyssey (customer data files) and CloudVault.
 
-This script currently only populates field groups, fields (plots) with geometries, and planting dates.
-
-Data is sourced from local CSV files of customer field notes, as well as CloudVault.
+To run: `poetry run python3 -m demeter_utils.data_ingest --date_on_or_after 2023-05-01 --analytic_name "Plot Multispectral Indices and Uniformity"`
 """
 
 # TODO: This is currently picking up some `gql` INFO messaging with the logger. Might want to remove that.
-# TODO: Fix the fiona error. I think this is a result of the source file and not the process.
 
 import argparse
 import logging
+from ast import literal_eval
+from datetime import datetime
+from os import getenv
+from os.path import join
 
-from demeter.db import getConnection
 from dotenv import load_dotenv
+from geopandas import GeoDataFrame
+from pandas import merge as pd_merge
+from pandas import read_csv
 from utils.logging.tqdm import logging_init
 
-from demeter_utils.data_ingest._collect_data import collect_data, collect_ndvi_data
-from demeter_utils.data_ingest._insert_data import insert_data
+from demeter_utils.data_ingest._collect_data import load_field_insights_data
 
 if __name__ == "__main__":
     """Main function."""
     c = load_dotenv()
     logging_init()
-
     parser = argparse.ArgumentParser(
-        description="Collect and insert Bayer Canola data into Demeter schema."
+        description="Load and organize FIRST Seed Tests data from Odyssey and CloudVault."
     )
-
     parser.add_argument(
-        "--database_host",
+        "--date_on_or_after",
         type=str,
-        help="Host of demeter database; can be 'AWS' or 'LOCAL'.",
-        default="LOCAL",
+        help="Earliest date to load data from.",
+        default="2023-05-01",
     )
-
     parser.add_argument(
-        "--database_env",
+        "--analytic_name",
         type=str,
-        help="Database instance; can be 'DEV' or 'PROD'.",
-        default="DEV",
+        help="Name of analytic to load from CloudVault.",
+        default="Plot Multispectral Indices and Uniformity",
     )
+    parser.add_argument(
+        "--filepath_exp_design",
+        type=str,
+        help="Filename containing the `df_exp_design` information.",
+    )
+    # parser.add_argument(
+    #     "--database_host",
+    #     type=str,
+    #     help="Host of demeter database; can be 'AWS' or 'LOCAL'.",
+    #     default="LOCAL",
+    # )
+    # parser.add_argument(
+    #     "--database_env",
+    #     type=str,
+    #     help="Database instance; can be 'DEV' or 'PROD'.",
+    #     default="DEV",
+    # )
+
+    # date_on_or_after = datetime(2023, 5, 1)
+    # analytic_name = "Plot Multispectral Indices and Uniformity"
+    # database_host = "LOCAL"
+    # database_env = "DEV"
 
     # set up args
     args = parser.parse_args()
-    database_host = args.database_host
-    database_env = args.database_env
+    date_on_or_after = datetime.strptime(args.date_on_or_after, "%Y-%m-%d")
+    analytic_name = args.analytic_name
+    filepath_exp_design = args.filepath_exp_design
+    # database_host = args.database_host
+    # database_env = args.database_env
 
-    ssh_env_name = f"SSH_DEMETER_{database_host}" if database_host == "AWS" else None
-    database_env_name = f"DEMETER-{database_env}_{database_host}"
+    # ssh_env_name = f"SSH_DEMETER_{database_host}" if database_host == "AWS" else None
+    # database_env_name = f"DEMETER-{database_env}_{database_host}"
 
-    logging.info("Collecting field data")
-    gdf_plot, file_metadata = collect_data()
+    logging.info("Collecting field data from Odyssey")
+    primary_keys = ["site_name", "plot_id"]
 
-    logging.info("Collecting NDVI data from CloudVault")
-    df_ndvi = collect_ndvi_data(gdf_plot=gdf_plot)
+    df_exp_design = read_csv(filepath_exp_design)
 
-    logging.info("Connecting to database.")
-    conn = getConnection(env_name=database_env_name, ssh_env_name=ssh_env_name)
+    logging.info("Collecting multispectral data from CloudVault")
+    # TODO: How to load in the asset_sentera_id_dict?
+    ASSET_SENTERA_ID = literal_eval(getenv("ASSET_SENTERA_ID"))
+    gdf_plots, df_long = load_field_insights_data(
+        asset_sentera_id_dict=ASSET_SENTERA_ID,
+        date_on_or_after=date_on_or_after,
+        analytic_name=analytic_name,
+    )
 
-    logging.info("Inserting data to database.")
-    insert_data(conn, gdf_plot, file_metadata, df_ndvi)
+    gdf_exp_design = GeoDataFrame(
+        pd_merge(df_exp_design, gdf_plots, on=primary_keys, how="inner"),
+        geometry=gdf_plots.geometry.name,
+        crs=gdf_plots.crs,
+    )
+    # gdf_exp_design[pd.isnull(gdf_exp_design.geometry)]
+    if (len(df_exp_design) != len(gdf_plots)) | (len(gdf_exp_design) != len(gdf_plots)):
+        raise RuntimeError(
+            "Number of plots from experimental design CSVs and CloudVault GEOJSONs do not match."
+        )
 
-    conn.close()
+    # df_long_meta = pd_merge(
+    #     gdf_exp_design.loc[:, gdf_exp_design.columns != gdf_exp_design.geometry.name],
+    #     df_long,
+    #     on=primary_keys,
+    #     how="inner",
+    # )
+
+    data_dir = join(str(getenv("DEMETER_DIR")), "projects/mosaic_co_stats/data")
+
+    logging.info("Saving gdf_exp_design.%s to Odyssey...", "parquet")
+    gdf_exp_design.to_parquet(join(data_dir, "gdf_exp_design.parquet"))
+
+    logging.info("Saving gdf_exp_design.%s to Odyssey...", "geojson")
+    gdf_exp_design.to_file(join(data_dir, "gdf_exp_design.geojson"), driver="GeoJSON")
+
+    logging.info("Saving df_long.%s to Odyssey...", "parquet")
+    df_long.to_parquet(join(data_dir, "df_long.parquet"))
+
+    # TODO: Load into database
+    # logging.info("Connecting to database.")
+    # conn = getConnection(env_name=database_env_name, ssh_env_name=ssh_env_name)
+
+    # logging.info("Inserting data to database.")
+    # insert_data(conn, gdf_plot, file_metadata, df_ndvi)
+
+    # conn.close()
 
     logging.info("Import complete.")
--- demeter_utils/data_ingest/_collect_data.py
+++ demeter_utils/data_ingest/_collect_data.py
@@ -1,79 +1,203 @@
 import logging
 from datetime import datetime
+from typing import Tuple
 
-from geopandas import GeoDataFrame
+from geo_utils.field_trials import set_plot_id
+from geopandas import GeoDataFrame, read_file
 from pandas import DataFrame
 from pandas import concat as pd_concat
-from pandas import merge as pd_merge
+from pandas import melt
 
-from demeter_utils.data_ingest._utils import (
-    get_cv_connection,
-    get_ndvi_plot_ratings_for_asset,
-)
-from demeter_utils.data_ingest.constants import ASSET_SENTERA_ID
+from demeter_utils.data_ingest._utils import get_asset_analytic_info, get_cv_connection
 
 
-def collect_ndvi_data(gdf_plot: GeoDataFrame) -> DataFrame:
-    """Collect and organize NDVI Plot Ratings data for the Bayer Canola Phase 1 project.
+def _parse_stat_name(x: str) -> str:
+    """Parse statistic name from variable name string and return statistic name."""
+    stat_list = ["Mean", "Median", "Mode", "Std. Dev.", "Min", "Max", "Range"]
+    for stat in stat_list:
+        if stat.casefold() in x.casefold():
+            return stat
+    return "Mean"  # https://catalog.sentera.com/products/plot_crop_health_multispectral_uniformity doesn't specify "Mean"
 
-    Extract data from all available NDVI Plot Ratings feature sets after the earliest planting date for each site.
 
-    Gathered data and data sources:
-    1. NDVI Plot Ratings
-    --- Plot-level mean NDVI from drone imagery
+def _wide_to_long(
+    df: DataFrame, primary_keys: list, value_subset: list, crop_season_year: int
+) -> DataFrame:
+    """Convert wide-format dataframe to long-format dataframe, parsing identifier columns."""
+    df_long = melt(
+        df,
+        id_vars=primary_keys,
+        value_vars=value_subset,
+    )
+    idx_initial_insert = 3  # column index of first column insertion
+    df_long.insert(
+        idx_initial_insert,
+        "date",
+        df_long["variable"].apply(
+            lambda x: datetime.strptime(
+                x.split(" ")[-1] + f"-{crop_season_year}", "%d-%b-%Y"
+            ).date()
+        ),
+    )
+    df_long.insert(
+        idx_initial_insert + 1,
+        "observation_type",
+        df_long["variable"].apply(lambda x: " ".join(x.split(" ")[:2])),
+    )
+    df_long.insert(
+        idx_initial_insert + 2,
+        "statistic_type",
+        df_long["variable"].apply(lambda x: _parse_stat_name(x)),
+    )
+    df_long.insert(
+        idx_initial_insert + 3,
+        "subplot",
+        df_long["variable"].apply(lambda x: True if "subplot" in x.lower() else False),
+    )
+    return df_long
+
+
+# def load_trial_info() -> DataFrame:
+#     """Load and organize treatment information.
+
+#     Gathered data and data sources:
+#     1. Field trial information for each site/location (CSV)
+
+#     Returns:
+#         df_exp_design (DataFrame): Trial information for all sites, including `plot_id` and `seed_id`. Does not include
+#             plot geometry.
+#     """
+#     logging.info("   Collecting and cleaning field notes data from CSV files")
+
+#     demeter_dir = str(getenv("DEMETER_DIR"))
+#     data_dir = join(demeter_dir, "projects/mosaic_co_stats/data")
+
+#     df_exp_design = None
+#     for asset_name in ASSET_SENTERA_ID.keys():
+#         # Load plot data
+#         fname_seed_id = join(data_dir, f"seed_id - {asset_name}.csv")
+#         df_temp = read_csv(fname_seed_id).rename(columns={"ms": "plot_id"})
+
+#         # clean up and standardize
+#         df_temp.insert(0, "site_name", asset_name)
+#         df_exp_design = (
+#             pd_concat([df_exp_design, df_temp], axis=0, ignore_index=True)
+#             if df_exp_design is not None
+#             else df_temp.copy()
+#         )
+#     return df_exp_design
+
+
+def load_field_insights_data(
+    asset_sentera_id_dict: dict,
+    date_on_or_after: datetime = datetime(2023, 5, 1),
+    analytic_name: str = "Plot Multispectral Indices and Uniformity",
+) -> Tuple[GeoDataFrame, dict]:
+    """Load and organize Sentera FieldInsights-formatted deliverables/data.
 
     Args:
-        gdf_plot: See collect_data() below for more information.
+        asset_sentera_id_dict (dict): Sentera asset names and corresponding sentera_ids.
+        date_on_or_after (datetime): Earliest date to load data from; defaults to May 1, 2023.
+
+        analytic_name (str): Name of analytic to load from CloudVault; could be `None` (?), in which case all available
+            geojson analytics will be loaded. Defaults to "Plot Multispectral Indices and Uniformity".
+
+    From CloudVault, all available "Plot Multispectral Indices and Uniformity" GeoJSONs for an asset are used.
 
     Returns:
-        df_ndvi (DataFrame) long-format dataframe with plot-level mean NDVI values from all available NDVI Plot
-            Ratings geojson files
+        gdf_plots (GeoDataFrame): Plot boundaries and metadata for all sites, including plot_id, site_name, row, range,
+            and geometry.
+
+        df_long (DataFrame): long-format dataframe containing plot-level FieldInsights data for all sites, plots, and
+            observations.
     """
-    logging.info("   Getting observed NDVI plot ratings from CloudVault")
+    logging.info('   Checking for valid "%s" layers from CloudVault', analytic_name)
     client, ds = get_cv_connection()
 
-    df_obs = None
-    for asset_name in ASSET_SENTERA_ID.keys():
-        print(asset_name)
-
-        # get earliest planting date
-        inds = gdf_plot["site"] == asset_name
-        date_plant_earliest = (
-            gdf_plot.loc[inds, "date_planted"]
-            .map(lambda d: datetime.strptime(d, "%m/%d/%Y"))
-            .min()
+    df_analytic_list = None
+    for asset_name in asset_sentera_id_dict.keys():
+        df_analytic_asset = get_asset_analytic_info(
+            client,
+            ds,
+            asset_sentera_id=asset_sentera_id_dict[asset_name],
+            date_on_or_after=date_on_or_after,
+            analytic_name=analytic_name,
+            file_type="geo_json",
         )
-        asset_id = ASSET_SENTERA_ID[asset_name]
 
-        # get observed plot-level mean NDVI from CloudVault
-        df_obs_temp = get_ndvi_plot_ratings_for_asset(
-            client, ds, asset_id, date_plant_earliest
-        )
-        df_obs_temp.insert(0, "site", asset_name)
-
-        if df_obs is None:
-            df_obs = df_obs_temp.copy()
-        else:
-            df_obs = pd_concat([df_obs, df_obs_temp], axis=0, ignore_index=True)
-
-    # the new script for processing plot ratings does not include Sentera ID :(
-    complete = df_obs["sentera_id"] > 0
-    df_sentera_id = df_obs.loc[
-        complete, ["site", "range", "column", "sentera_id"]
-    ].drop_duplicates()
-
-    len_check = len(df_obs)
-    df_obs = pd_merge(
-        df_obs.drop(columns=["sentera_id"]),
-        df_sentera_id,
-        on=["site", "range", "column"],
-    )
-    assert len(df_obs) == len_check, "Re-merge for Sentera ID didn't work."
-
-    df_reduced = pd_merge(
-        df_obs,
-        gdf_plot[["site", "sentera_id", "range", "column"]],
-        on=["site", "sentera_id", "range", "column"],
+        if df_analytic_asset is not None:
+            df_analytic_asset.insert(0, "site_name", asset_name)
+            df_analytic_list = (
+                pd_concat(
+                    [df_analytic_list, df_analytic_asset], axis=0, ignore_index=True
+                )
+                if df_analytic_list is not None
+                else df_analytic_asset.copy()
+            )
+    df_analytic_list.drop_duplicates(inplace=True)
+    logging.info(
+        "   Found %s analytics from %s surveys",
+        len(df_analytic_list),
+        len(df_analytic_list["survey_sentera_id"].unique()),
     )
 
-    return df_reduced
+    gdf_plots = None  # Load field boundary data
+    df_long = None
+    # For wide to long conversion
+    primary_keys = ["site_name", "plot_id"]
+    cols_to_remove = [
+        "trial_id",
+        "range",
+        "row",
+        "num_rows",
+        "stroke",
+        "stroke-opa",
+        "fill",
+        "fill-opaci",
+        "geometry",
+    ]
+
+    # Wide to long format for all sites, surveys, and plots
+    for i, row in df_analytic_list.iterrows():
+        logging.info(
+            '   %s: Loading data from CloudVault: %s "%s"',
+            i,
+            row["date"].date(),
+            row["site_name"],
+        )
+        gdf_temp = read_file(row["url"])
+        # Overwrite incorrect plot_id values
+        if (
+            row["site_name"] == "Jefferson"
+        ):  # Plot IDs were incorrectly assigned; must increase from W to E THEN S to N
+            gdf_temp["plot_id"] = gdf_temp.apply(
+                lambda x: set_plot_id(first=x["row"], last=x["range"], zfill=2), axis=1
+            )
+        gdf_temp.insert(0, "site_name", row["site_name"])
+
+        gdf_plots_temp = gdf_temp[["site_name", "plot_id", "geometry"]].copy()
+
+        value_subset = list(
+            filter(
+                lambda x: x not in primary_keys + cols_to_remove, list(gdf_temp.columns)
+            )
+        )
+        df_long_temp = _wide_to_long(
+            gdf_temp[primary_keys + value_subset],
+            primary_keys,
+            value_subset,
+            crop_season_year=row["date"].year,
+        )
+
+        gdf_plots = (
+            pd_concat([gdf_plots, gdf_plots_temp], axis=0, ignore_index=True)
+            if gdf_plots is not None
+            else gdf_plots_temp.copy()
+        )
+        df_long = (
+            pd_concat([df_long, df_long_temp], axis=0, ignore_index=True)
+            if df_long is not None
+            else df_long_temp.copy()
+        )
+    gdf_plots.drop_duplicates(subset=primary_keys, inplace=True)
+    return gdf_plots, df_long
