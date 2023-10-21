fd70170d9dc4bcbc0599aea306e9f7226090c8b8
tnigon <8249296+tnigon@users.noreply.github.com>
2023-10-20 19:10:27 -0500

    add double logistic

--- demeter_utils/time_series/inference/__init__.py
+++ demeter_utils/time_series/inference/__init__.py
@@ -1,10 +1,9 @@
-from ._inference import TimeSeriesFitter, TimeSeriesParams
+from ._inference import TimeSeriesFitter
 from ._prep import get_df_skeleton
 from ._utils import populate_fill_in_values
 
 __all__ = [
     "TimeSeriesFitter",
-    "TimeSeriesParams",
     "get_df_skeleton",
     "populate_fill_in_values",
 ]
--- demeter_utils/time_series/inference/_inference.py
+++ demeter_utils/time_series/inference/_inference.py
@@ -1,12 +1,17 @@
 from dataclasses import dataclass, field
-from datetime import date, timedelta
-from functools import cached_property
+from datetime import datetime, timedelta
+from functools import cached_property, partial
 from typing import Callable
 
 from pandas import DataFrame
 from scipy.interpolate import UnivariateSpline
+from scipy.optimize import minimize
 
 from demeter_utils.time import convert_dt_to_unix
+from demeter_utils.time_series.inference._double_logistic import (
+    approximate_inflection_with_cubic_poly,
+    double_logistic,
+)
 from demeter_utils.time_series.interpolate import weighted_moving_average
 
 
@@ -109,7 +114,7 @@ class TimeSeriesFitter:
         # Return the callable
         if callable_unit_datetime:
 
-            def get_value_from_datetime(dt: date) -> float:
+            def get_value_from_datetime(dt: datetime) -> float:
                 t = convert_dt_to_unix(
                     dt, relative_epoch=self.df_daily_weighted_moving_avg["t"].min()
                 )
@@ -123,11 +128,106 @@ class TimeSeriesFitter:
         self,
     ) -> Callable:
         """
-        _summary_
+        Fits double logistic function to the smoothed and weighted data values.
 
         Returns:
             Callable: Takes a `datetime` value as an argument and returns the estimated data value based on the fitted
                 smoothing spline.
         """
-        # TODO: Implement double logistic fitting method
-        pass
+        # UnivariateSpline must take `int`` dtype (i.e., unix) for `x`
+        xt = convert_dt_to_unix(
+            self.df_daily_weighted_moving_avg["t"],
+            relative_epoch=self.df_daily_weighted_moving_avg["t"].min(),
+        ).astype(float)
+        y = self.df_daily_weighted_moving_avg["y"].astype(float)
+
+        ymax = self.df[self.col_value].max()
+        ymin = self.df[self.col_value].min()
+
+        # Determine left and right side of curve
+        max_threshold = 0.1
+        max_bound = ymax * (1 - max_threshold)
+        ind_max = self.df.loc[self.df[self.col_value] >= max_bound].index.values
+
+        # Approximate inflection points
+        df_left = self.df.iloc[: min(ind_max) + 1, :]
+        left_params = approximate_inflection_with_cubic_poly(
+            t=df_left[self.col_datetime],
+            y=df_left[self.col_value],
+            ymin=ymin,
+            ymax=ymax,
+        )
+        df_right = self.df.iloc[max(ind_max) :, :]
+        right_params = approximate_inflection_with_cubic_poly(
+            t=df_right[self.col_datetime],
+            y=df_right[self.col_value],
+            ymin=ymin,
+            ymax=ymax,
+        )
+
+        guess = {
+            "ymin": ymin,
+            "ymax": ymax,
+            "t_incr": left_params["t"],
+            "t_decr": right_params["t"],
+            "rate_incr": left_params["rate"],
+            "rate_decr": right_params["rate"],
+        }
+        guess_values = [*guess.values()]
+
+        # Define cost function
+        def _cost_function(p, xt, y):
+            partial_fx = partial(
+                double_logistic,
+                ymin=p[0],
+                ymax=p[1],
+                t_incr=p[2],
+                t_decr=p[3],
+                rate_incr=p[4],
+                rate_decr=p[5],
+            )
+            y_pred = partial_fx(xt)
+            se = (y_pred - y) ** 2
+            return se.sum()
+
+        # Minimize cost function with initial values
+        opt = minimize(_cost_function, guess_values, args=(xt, y))
+        popt = opt.x
+        pars = {
+            "ymin": popt[0],
+            "ymax": popt[1],
+            "t_incr": popt[2],
+            "t_decr": popt[3],
+            "rate_incr": popt[4],
+            "rate_decr": popt[5],
+        }
+
+        # Standardize to reduce scale (mean = 0, sd = 1)
+        date_min = self.df[self.col_datetime].min()
+        s_unix = self.df[self.col_datetime].map(
+            lambda dt: convert_dt_to_unix(dt, relative_epoch=date_min)
+        )
+        t_mean = s_unix.mean()
+        t_sd = s_unix.std()
+
+        # Define the datetime to unix conversion to embed into get_value_from_datetime()
+        def dt_transformation(dt: datetime) -> float:
+            unix = convert_dt_to_unix(
+                dt, relative_epoch=self.df_daily_weighted_moving_avg["t"].min()
+            )  # convert to psuedo-unix
+            return (unix - t_mean) / t_sd  # scale
+
+        # Create partial function that takes `datetime`, transforms it appropriately, and estimates value
+        def get_value_from_datetime(dt: datetime) -> float:
+            get_value_from_relative_epoch_fx = partial(
+                double_logistic,
+                ymin=pars["ymin"],
+                ymax=pars["ymax"],
+                t_incr=pars["t_incr"],
+                t_decr=pars["t_decr"],
+                rate_incr=pars["rate_incr"],
+                rate_decr=pars["rate_decr"],
+            )
+            return get_value_from_relative_epoch_fx(dt_transformation(dt))
+
+        return get_value_from_datetime
