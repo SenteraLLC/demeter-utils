6fd4ec4b283d08eb55469c0bf8e01feda7e86121
tnigon <8249296+tnigon@users.noreply.github.com>
2023-11-07 17:22:28 -0600

    add load_exp_design() to init

--- demeter_utils/data_ingest/__init__.py
+++ demeter_utils/data_ingest/__init__.py
@@ -1,3 +1,4 @@
+from demeter_utils.data_ingest._local import load_exp_design
 from demeter_utils.data_ingest._time import get_unix_from_datetime
 
-__all__ = ["get_unix_from_datetime"]
+__all__ = ["get_unix_from_datetime", "load_exp_design"]
