463aa97ae6fdd8f762586e5fcf3c8b5dff62ccbe
tnigon <8249296+tnigon@users.noreply.github.com>
2023-12-08 16:19:27 -0600

    join_crop_type ad back reorder

--- demeter_utils/query/demeter/_crop_type.py
+++ demeter_utils/query/demeter/_crop_type.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from pandas import DataFrame
 
+from demeter_utils.query._translate import reorder_dataframe_columns
 from demeter_utils.query.demeter._core import basic_demeter_query
 
 
@@ -18,4 +19,9 @@ def join_crop_type(cursor: Any, df: DataFrame) -> DataFrame:
         conditions={"crop_type_id": crop_type_ids},
         explode_details=True,
     ).drop(columns=["created", "last_updated"])
-    return df.merge(df_crop, how="left", on="crop_type_id")
+    df_out = df.merge(df_crop, how="left", on="crop_type_id")
+
+    cols_to_reorder = reversed(df_crop.drop(columns="crop_type_id").columns)
+    return reorder_dataframe_columns(
+        df_out, col_to_insert_after="crop_type_id", cols_to_reorder=cols_to_reorder
+    )
