9235ea6be47143a2a5bb877766cf7241e39f9a33
tnigon <8249296+tnigon@users.noreply.github.com>
2023-12-14 10:58:47 -0600

    move insert_act() to demeter_utils

--- demeter_utils/insert/demeter/__init__.py
+++ demeter_utils/insert/demeter/__init__.py
@@ -1,3 +1,4 @@
+from demeter_utils.insert.demeter._act import insert_act
 from demeter_utils.insert.demeter._core import DEMETER_IDS
 
-__all__ = ["DEMETER_IDS"]
+__all__ = ["DEMETER_IDS", "insert_act"]
